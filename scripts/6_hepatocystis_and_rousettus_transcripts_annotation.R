fa9a016628fbf89b9918704407943c39bc0b1250
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2023-09-27 13:40:12 +0200

    Moving the annotation to the correct scritp and saving it

--- scripts/6_hepatocystis_and_rousettus_transcripts_annotation.R
+++ scripts/6_hepatocystis_and_rousettus_transcripts_annotation.R
@@ -1,9 +1,17 @@
-# annotation of hepatocystis transcripts 
+## annotation of transcripts:
+
+## Hepatocystis transcripts are queried here and annotations are
+## printed to the console
+
+## Bat GO term transcript annotations are obtained from the ENSEMBLE
+## biomart and stored in an intermediated file 
+
 
 library(rtracklayer)
 library(biomaRt)
- 
-# rerun script 1 for generating counts table or read from intermediate data
+
+## rerun script 1 for generating counts table or read from
+## intermediate data
 readcount <- FALSE
 
 # loading the dataframe (file containing the read counts) from the features count step
@@ -34,67 +42,13 @@ rousettus_proteins <- read.csv("inputdata/rousettus_proteins.csv")
 merged_gtf <- readGFF("/SAN/RNASeqHepatoCy/HostTranscriptome/host_merged_Raegyptiacus_and_HepatocystisAunin/STAR/featurescount/RousettusHepatocystis_merged.gtf")
 
 
-biomaRt::listMarts()
-?listMarts
-
-mart <- useMart(biomart = "ENSEMBL_MART_ENSEMBL", 
-                dataset = "hsapiens_gene_ensembl")
-
-biomaRt::listDatasets(mart)
-biomaRt::listAttributes(mart)
-# get dataset and GO terms
-go_list <- getBM(attributes=c("go_id"),
-                 filters = "go",
-                 mart=mart)
-
-
-
-# our hepatocystis transcripts with >5 counts
-tagseqRNAfeatureCounts %>% 
-  filter(grepl("HEP_",rownames(tagseqRNAfeatureCounts),ignore.case = TRUE),
-         rowSums(tagseqRNAfeatureCounts)>5)
-
-rowSums(tagseqRNAfeatureCounts %>% 
-          filter(grepl("HEP_",rownames(tagseqRNAfeatureCounts),ignore.case = TRUE),
-                 rowSums(tagseqRNAfeatureCounts)>5))
-
-# rousettus transcripts
-tagseqRNAfeatureCounts %>% 
-  filter(!grepl("HEP_",rownames(tagseqRNAfeatureCounts),ignore.case = TRUE),
-         rowSums(tagseqRNAfeatureCounts)>5)
-
-
-# are our 29 hepatocystis transcripts IDs present in the hepatocystis proteins table from NCBI
-all(tagseqRNAfeatureCounts %>% 
-  filter(grepl("HEP_",rownames(tagseqRNAfeatureCounts),ignore.case = TRUE),
-         rowSums(tagseqRNAfeatureCounts)>5) == hepatocystis_proteins$Locus.tag)
-            # they are not
-
-# check if our feature counts gene IDs are in the merged gtf file
-table(rownames(tagseqRNAfeatureCounts) %in% merged_gtf$gene_id)
-
-# check the genes present in the merged gtf that our transcripts were mapped to
-table(merged_gtf$gene_id %in% rownames(tagseqRNAfeatureCounts))
-
 merged_gtf <- merged_gtf[merged_gtf$gene_id %in% rownames(tagseqRNAfeatureCounts),]
-colnames(merged_gtf)
 
-# exploring the data 
-
-# number of transcripts according to type and biotype
-table(merged_gtf$type)
-table(merged_gtf$gene_biotype)
 
 # adding species column to the gtf file
 merged_gtf$species <- ifelse(grepl("HEP_", merged_gtf$gene_id),
                              "hepatocystis",
                              "rousettus")
-
-# type, biotype and locus tag of transcripts according to species
-table(merged_gtf$type, merged_gtf$species)
-table(merged_gtf$gene_biotype, merged_gtf$species)
-table(merged_gtf$locus_tag, merged_gtf$species)  #locus tag is only in hepatocystis transcripts
-
 
 # mitochondrial genome not mapped to by our transcripts  
 rowSums(tagseqRNAfeatureCounts %>% 
@@ -102,15 +56,9 @@ rowSums(tagseqRNAfeatureCounts %>%
                rownames(tagseqRNAfeatureCounts),
                ignore.case = TRUE)))
 
+## numeric(0)
 sum(grepl("HEP_MIT", rownames(tagseqRNAfeatureCounts), ignore.case = TRUE))
-
-# motochondrial genome in the assembly (annotation file)
-rowSums(merged_gtf %>% 
-          filter(grepl("HEP_MIT",
-                       merged_gtf$gene_id,
-                       ignore.case = TRUE)))
-
-sum(grepl("HEP_MIT", merged_gtf$gene_id, ignore.case = TRUE))
+## [1] 0
 
 
 # what the highest expressed hepatocystis transcript is
@@ -126,7 +74,9 @@ merged_gtf %>%
          rowSums(tagseqRNAfeatureCounts)>5))),
 select = c(gene_id,gene_biotype,product)) %>%
   as.data.frame()
-  
+## they are all rRNA
+
+
 # what our rousettus transcripts are
 merged_gtf %>%
   subset(gene_id %in%
@@ -137,47 +87,30 @@ merged_gtf %>%
   rowSums(tagseqRNAfeatureCounts)>5))),
   select = c(gene_id,gene_biotype,product)) %>%
   as.data.frame()
-
+## proper protein coding genes
 
 # selecting columns of interest in the merged gtf file
 merged_gtf <- merged_gtf[c("gene_id")]
                              
 
-# what is only expressed by our transcripts                             
-# what proteins our hepatocystis transcripts translate to
+mart <- useMart(biomart = "ENSEMBL_MART_ENSEMBL",
+                dataset="hsapiens_gene_ensembl")
 
-rownames(tagseqRNAfeatureCounts %>% 
-      filter(grepl("HEP_",rownames(tagseqRNAfeatureCounts),ignore.case = TRUE),
-             rowSums(tagseqRNAfeatureCounts)>5)) %in% 
-        merged_gtf$gene_id %in% 
-  hepatocystis_proteins$Locus.tag
-      # the transcripts ids are not present in the proteins table
+### query a human biomart for the gene symbols available in the
+### "locus" column of the protein table
 
-# alternatively
-hepatocystis_proteins %>%
-filter(Locus.tag %in%
-  merged_gtf$gene_id[merged_gtf$gene_id %in%
-  (rownames(tagseqRNAfeatureCounts %>% 
-           filter(grepl("HEP_",rownames(tagseqRNAfeatureCounts),ignore.case = TRUE),
-                  rowSums(tagseqRNAfeatureCounts)>0)))])
-       
-# annotations for rousettus transcripts       
-rousettus_proteins %>%
-  filter(Locus %in%
-  merged_gtf$gene_id[merged_gtf$gene_id %in%
- (rownames(tagseqRNAfeatureCounts %>% 
- filter(!grepl("HEP_",rownames(tagseqRNAfeatureCounts),ignore.case = TRUE),
- rowSums(tagseqRNAfeatureCounts)>0)))])
+### Those are usually human (universal) gene symbols. Only in a few
+### cases they are real non-transferrable locus idetnifers. We don't
+### query those
 
-# selecting columns from the proteins annotations table
-as.data.frame(rousettus_proteins %>%
-  filter(Locus %in%
-  merged_gtf$gene_id[merged_gtf$gene_id %in%
-  (rownames(tagseqRNAfeatureCounts %>% 
-  filter(!grepl("HEP_",rownames(tagseqRNAfeatureCounts),ignore.case = TRUE),
-  rowSums(tagseqRNAfeatureCounts)>0)))]) %>%
-  dplyr::select(Accession, Locus, Protein.Name))
+rousettus_proteins <-  rousettus_proteins[!grepl("^LOC", rousettus_proteins$Locus), ]
 
-                             
+allLocusGO <- getBM(mart=mart,
+                    attributes = c("entrezgene_accession",
+                                   "entrezgene_id",
+                                   "entrezgene_description",
+                                   "go_id"),
+                    filters =  "entrezgene_accession",
+                    values = rousettus_proteins$Locus) ###all values in "Locus"
 
-colnames(metadata)
+saveRDS(allLocusGO, "intermediateData/GOtermAnnot.RDS")
